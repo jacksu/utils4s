4a31873287d89885b181d0291a9d8008aa58b1ec
jacksu <371387455@qq.com>
2015-10-08 20:02:46 +0800

    modify pom.xml for module

--- log-demo/src/main/scala/cn/thinkjoy/scala4fun/App.scala
+++ log-demo/src/main/scala/cn/thinkjoy/scala4fun/App.scala
@@ -5,7 +5,10 @@ import org.log4s._
  * Hello world!
  *
  */
+
 object App {
+  case class ClassWithPrivateFields(name: String)
+
   def main(args: Array[String]) {
     val test=new LoggingTest
     test.logPrint()
@@ -15,6 +18,9 @@ object App {
     log.info("info log")
     log.warn("warn log")
     log.error("error log")
+
+    val aClass = new ClassWithPrivateFields("sname")
+    println(aClass.name)
   }
 
 }
