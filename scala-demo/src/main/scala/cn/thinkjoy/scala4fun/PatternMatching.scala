34c6b4c49c76fdffb127e09466273167727bb80b
jacksu <371387455@qq.com>
2015-10-17 15:37:46 +0800

    add scala demo

---
+++ scala-demo/src/main/scala/cn/thinkjoy/scala4fun/PatternMatching.scala
@@ -0,0 +1,56 @@
+package cn.thinkjoy.scala4fun
+
+/**
+ * Pattern Matching
+ *
+ */
+
+object PatternMatching {
+  def matchTest(x: Int) = x match {
+    case 1 => "One"
+    case 2 => "Two"
+    case _ => "Other"
+  }
+
+  def goldilocks(expr: Any) = expr match {
+    case ("porridge", "Papa") => "Papa eating porridge"
+    case ("porridge", _) => "Mama eating porridge"
+    case ("porridge", "Baby") => "Baby eating porridge"
+    case _ => "what?"
+  }
+
+  def expression(expr: Any) = expr match {
+    case ("porridge", bear) => bear + " said someone's been eating my porridge"
+    case ("chair", bear) => bear + " said someone's been sitting in my chair"
+    case ("bed", bear) => bear + " said someone's been sleeping in my bed"
+    case _ => "what?"
+  }
+
+  def main(args: Array[String]) {
+    println(matchTest(3))
+
+
+    val stuff = "blue"
+    val myStuff = stuff match {
+      case "red" => println("RED"); 1
+      case "blue" => println("BLUE"); 2
+      case "green" => println("GREEN"); 3
+      case _ => println(stuff); 0 //case _ will trigger if all other cases fail.
+    }
+    assert(myStuff == 2)
+
+    val complex = stuff match {
+      case "red" => (255, 0, 0)
+      case "green" => (0, 255, 0)
+      case "blue" => (0, 0, 255)
+      case _ => println(stuff); 0
+    }
+    assert(complex == (0,0,255))
+
+    //模式匹配通配符
+    assert(goldilocks(("porridge", "Mama")) == "Mama eating porridge")
+
+    //模式匹配代替表达式
+    println(expression( ("chair", "jack")))
+  }
+}
