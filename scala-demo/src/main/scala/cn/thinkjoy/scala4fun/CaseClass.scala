091ce8f92111e90d381759068f9731928cbe83fc
jacksu <371387455@qq.com>
2015-10-25 21:37:22 +0800

    update scala excerice

---
+++ scala-demo/src/main/scala/cn/thinkjoy/scala4fun/CaseClass.scala
@@ -0,0 +1,49 @@
+package cn.thinkjoy.scala4fun
+
+/**
+ * Created by jacksu on 15/10/24.
+ */
+
+abstract class Term
+case class  Var(name: String) extends Term
+case class Fun(arg: String, body: Term) extends Term
+case class App(f: Term, v: Term) extends Term
+case class Dog(name: String, breed: String) // Doberman
+
+object CaseClass {
+  def main(args: Array[String]) {
+    def printTerm(term: Term) {
+      term match {
+        case Var(n) =>
+          print(n)
+        case Fun(x, b) =>
+          print("^" + x + ".")
+          printTerm(b)
+        case App(f, v) =>
+          Console.print("(")
+          printTerm(f)
+          print(" ")
+          printTerm(v)
+          print(")")
+      }
+    }
+    def isIdentityFun(term: Term): Boolean = term match {
+      case Fun(x, Var(y)) if x == y => true
+      case _ => false
+    }
+    val id = Fun("x", Var("x"))
+    val t = Fun("x", Fun("y", App(Var("x"), Var("y"))))
+    printTerm(t)
+    println
+    println(isIdentityFun(id))
+    println(isIdentityFun(t))
+
+    val d1 = Dog("Scooby", "Doberman")
+
+    val d2 = d1.copy(name = "Scooby Doo") // copy the case class but change the name in the copy
+    println(d2.name)
+
+    val d3=Dog.unapply(d2).get
+    println(d3._1)
+  }
+}
---
+++ scala-demo/src/main/scala/cn/thinkjoy/scala4fun/Extract.scala
@@ -0,0 +1,15 @@
+package cn.thinkjoy.scala4fun
+
+/**
+ * Created by jacksu on 15/10/25.
+ */
+object Extract {
+  def main(args: Array[String]) {
+    val list = 1 :: 2 :: 3 :: 4 :: Nil
+    val extract = list match {
+      case List(first, second, _*) => first + second
+      case _ => 0
+    }
+    println(extract)
+  }
+}
