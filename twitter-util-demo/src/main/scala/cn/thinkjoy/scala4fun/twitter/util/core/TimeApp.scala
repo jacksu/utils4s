9c5545a1995ba8cfb2eca7520ced898d8c1cc0d3
jacksu <371387455@qq.com>
2015-11-18 20:33:27 +0800

    add twitter util time

---
+++ twitter-util-demo/src/main/scala/cn/thinkjoy/scala4fun/twitter/util/core/TimeApp.scala
@@ -0,0 +1,15 @@
+package cn.thinkjoy.scala4fun.twitter.util.core
+
+import com.twitter.conversions.time._
+
+object TimeApp {
+  def main(args: Array[String]) {
+    val duration1 = 1.second
+    val duration2 = 2.minutes
+    //duration1.inMillis
+    println( duration1.inMilliseconds )
+    println((duration2-duration1).inSeconds)
+
+  }
+
+}
