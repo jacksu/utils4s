646781c4ca28383cdfda5cf0ef34382c5374eb33
jacksu <371387455@qq.com>
2015-12-21 10:01:55 +0800

    新增breeze 操作

--- breeze-demo/src/main/scala/cn/thinkjoy/utils4s/breeze/BreezeApp.scala
+++ breeze-demo/src/main/scala/cn/thinkjoy/utils4s/breeze/BreezeApp.scala
@@ -1,7 +1,7 @@
 package cn.thinkjoy.utils4s.breeze
 
 //包含线性代数包（linear algebra）
-import breeze.linalg.{SparseVector, DenseVector}
+import breeze.linalg._
 
 /**
  * jacksu
@@ -33,6 +33,48 @@ object BreezeApp {
     println(x(3 to 4):=.5)
     //DenseVector(0.0, 2.0, 0.0, 0.5, 0.5)
     println(x)
+    println(x(1))
 
+    //==========DenseMatrix===========
+    /**
+     * 0  0  0  0  0
+     * 0  0  0  0  0
+     * 0  0  0  0  0
+     * 0  0  0  0  0
+     * 0  0  0  0  0
+     */
+    val m=DenseMatrix.zeros[Int](5,5)
+    println(m)
+
+    /**
+     * 向量是列式的
+     * 0  0  0  0  1
+     * 0  0  0  0  2
+     * 0  0  0  0  3
+     * 0  0  0  0  4
+     * 0  0  0  0  5
+     */
+    m(::,4):=DenseVector(1,2,3,4,5)
+    println(m)
+    //5
+    println(max(m))
+    //15
+    println(sum(m))
+    //DenseVector(1.0, 1.5, 2.0)
+    println(linspace(1,2,3))
+
+    //==========对角线============
+    /**
+     * 1.0  0.0  0.0
+     * 0.0  1.0  0.0
+     * 0.0  0.0  1.0
+     */
+    println(DenseMatrix.eye[Double](3))
+    /**
+     * 1.0  0.0  0.0
+     * 0.0  2.0  0.0
+     * 0.0  0.0  3.0
+     */
+    println(diag(DenseVector(1.0,2.0,3.0)))
   }
 }
